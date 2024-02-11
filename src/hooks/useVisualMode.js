2c7cb32a77f8318979bab0d59c381594293cd5f6
DustinFader <dustinfader16@gmail.com>
2024-02-10 22:57:40 -0400

    add a back fucntion to go back a mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,20 @@ import { useState } from "react";
 
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial)
+  const [history, setHistory] = useState([initial])
+
   const transition = (newMode) => {
-    setMode(prev => prev = newMode);
+    setMode(newMode);
+    setHistory(prev => [newMode, ...prev]);
   }
-  return {mode, transition};
+
+  const back = () => {
+    setMode(history[1]);
+    const [prev, ...rest] = history;
+    setHistory(prev => rest);
+  }
+  
+  return {mode, transition, back};
 };
 
 export default useVisualMode;
