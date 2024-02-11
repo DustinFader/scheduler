8d137ed4d4e868ce95d84bc8d8b0648f8d57c298
DustinFader <dustinfader16@gmail.com>
2024-02-10 22:27:48 -0400

    add trasition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,7 +2,10 @@ import { useState } from "react";
 
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial)
-  return {mode};
+  const transition = (newMode) => {
+    setMode(prev => prev = newMode);
+  }
+  return {mode, transition};
 };
 
 export default useVisualMode;
