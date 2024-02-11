0d710e3193fa0f73f0c7ec8adc79f8e4b4391e66
DustinFader <dustinfader16@gmail.com>
2024-02-10 23:43:31 -0400

    refactor to replace previous mode if replace in transition is true

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,23 +1,31 @@
-import { useState } from "react"; 
+import { useState } from "react";
 
 const useVisualMode = (initial) => {
-  const [mode, setMode] = useState(initial)
-  const [history, setHistory] = useState([initial])
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
     setMode(newMode);
-    setHistory(prev => [newMode, ...prev]);
-  }
+    setHistory((prev) => {
+      if (replace) {
+        const [first, ...rest] = prev;
+        return [newMode, ...rest];
+      }
+      return [newMode, ...prev];
+    });
+  };
 
   const back = () => {
-    setMode(history[1]);
-    setHistory(prev => {
-      const [first, ...rest] = prev;
-      return rest;
-    });
-  }
-  
-  return {mode, transition, back};
+    if (history.length > 1) {
+      setMode(history[1]);
+      setHistory((prev) => {
+        const [first, ...rest] = prev;
+        return rest;
+      });
+    }
+  };
+
+  return { mode, transition, back };
 };
 
 export default useVisualMode;
